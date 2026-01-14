1f93d9095cbfd34c04f361541ced82aa1f8103f3
Ricky9577 <riccardoalfano95@gmail.com>
2026-01-14 18:21:18 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = "dice-hoops-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.map((k) => (k !== CACHE_NAME ? caches.delete(k) : null)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  const req = event.request;
+  event.respondWith(
+    caches.match(req).then((cached) => {
+      if (cached) return cached;
+      return fetch(req)
+        .then((res) => {
+          // Cache solo richieste GET same-origin
+          try {
+            const url = new URL(req.url);
+            if (req.method === "GET" && url.origin === self.location.origin) {
+              const copy = res.clone();
+              caches.open(CACHE_NAME).then((cache) => cache.put(req, copy));
+            }
+          } catch {}
+          return res;
+        })
+        .catch(() => caches.match("./"));
+    })
+  );
+});
